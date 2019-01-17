3343a8c3881ebd960a3de841252522842bae815b
Attila Habensusz <attila.habensusz@gmail.com>
2019-01-17 14:08:00 +0100

    Corrected humpty-dumpty riddle

---
+++ Week-01/Day-03/humpty-dumpty.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Modify this program to console.log Humpty Dumpty riddle correctly
+
+console.log("Humpty Dumpty");
+console.log("Sat on a wall,");
+console.log("Humpty Dumpty");
+console.log("Had a great fall.");
+
+console.log("All the King's horses,");
+console.log("And all the King's men");
+console.log("Couldn't put Humpty");
+console.log("Together again.");
