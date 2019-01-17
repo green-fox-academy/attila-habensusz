58f9dc901cbd2408594255a38f4ff9be2fbfe1ae
Attila Habensusz <attila.habensusz@gmail.com>
2019-01-17 15:13:53 +0100

    Completed bmi exercise

---
+++ Week-01/Day-03/bmi.js
@@ -0,0 +1,5 @@
+'use strict';
+exports.__esModule = true;
+var massInKg = 81.2;
+var heightInM = 1.78;
+console.log(massInKg / heightInM);
---
+++ Week-01/Day-03/bmi.ts
@@ -0,0 +1,9 @@
+'use strict';
+export {};
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+console.log(massInKg / heightInM);
+
+// Print the Body mass index (BMI) based on these values
