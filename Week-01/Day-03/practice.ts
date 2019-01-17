ae98cfc88efcb31883d70ddcd5e360bc48c85473
Attila Habensusz <attila.habensusz@gmail.com>
2019-01-17 14:53:50 +0100

    Completed the assigment operator exercise

---
+++ Week-01/Day-03/practice.ts
@@ -0,0 +1,50 @@
+let welcome:string = "Hello there";
+console.log(welcome);
+
+let isAwesome: boolean = true;
+console.log(isAwesome);
+
+let iAmaNumbah: number = 100;
+console.log(iAmaNumbah);
+
+let pi: number = 3.141592;
+console.log(pi);
+
+iAmaNumbah = 90;
+
+let dynamic: any = true;
+dynamic = 5;
+
+let a: number = 50;
+a = a + 4;
+console.log(a);
+
+let b: number = 12;
+b = b - 4;
+console.log(b);
+
+let d: number = 12;
+d /= 3;
+console.log(d);
+
+let e: number = 30;
+e %= 7;
+console.log(e);
+
+let f: number = 12;
+f++;
+console.log(f);
+
+let g: number = 12;
+g--;
+console.log(g);
+
+let h: number = 10;
+console.log(h);
+console.log(h++);
+console.log(h);
+
+let i: number = 10;
+console.log(i);
+console.log(++i);
+console.log(i);
