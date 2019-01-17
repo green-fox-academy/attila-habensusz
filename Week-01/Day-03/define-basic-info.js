c694d053ea817303316a32594708c1c40c55b020
Attila Habensusz <attila.habensusz@gmail.com>
2019-01-17 15:22:59 +0100

    Completed basic info exercise

---
+++ Week-01/Day-03/define-basic-info.js
@@ -0,0 +1,11 @@
+'use strict';
+exports.__esModule = true;
+// Define several things as a variable, then print their values
+// Your name as a string
+var myName = "Atti";
+// Your age as an integer
+var myAge = 23;
+// Your height in meters as a float
+var myHeight = 1.73;
+// Whether you are married or not as a boolean
+var married = false;
---
+++ Week-01/Day-03/define-basic-info.ts
@@ -0,0 +1,15 @@
+'use strict';
+export {};
+
+// Define several things as a variable, then print their values
+// Your name as a string
+let myName = "Atti";
+
+// Your age as an integer
+let myAge = 23;
+
+// Your height in meters as a float
+let myHeight = 1.73;
+
+// Whether you are married or not as a boolean
+let married = false;
