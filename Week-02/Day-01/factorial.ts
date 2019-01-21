0d25772fa19b1a8e962801f5bc667cfb4be12d4f
Attila Habensusz <attila.habensusz@gmail.com>
2019-01-21 15:11:19 +0100

    Completed print task

--- Week-02/Day-01/factorial.ts
+++ Week-02/Day-01/factorial.ts
@@ -1,5 +1,6 @@
-function factorio(a) {
-  if (a == 0 || a == 1) {
+function factorio(a: number) {
+    //If statement for numbers that don't have factorials
+    if (a == 0 || a == 1) {
       return 1;
   } else {
     return factorio (a - 1) * a;
---
+++ Week-02/Day-01/print.ts
@@ -0,0 +1,7 @@
+function printParams() {
+    let arg = arguments;
+    for (let i = 0; i <arg.length; i++) {
+        console.log(arg[i]);
+    }
+}
+
