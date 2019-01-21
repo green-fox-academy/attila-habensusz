ce19da496440cb01d965a5349f916e0d482d96ad
Attila Habensusz <attila.habensusz@gmail.com>
2019-01-21 16:15:06 +0100

    Completed print-all exercise+

---
+++ Week-02/Day-01/print-all.ts
@@ -0,0 +1,11 @@
+let integers:number [] = [4,5,6,7];
+
+//With ES6 method
+integers.forEach(function(e){
+    console.log(e);
+});
+
+// Without the built-in method
+/*for (let i = 0; i <= integers.length; i++) {
+    console.log(integers[i]);
+}*/
