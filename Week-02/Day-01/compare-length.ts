79cf1d23736f621c2ef89d3f0afe4f0c46a4203d
Attila Habensusz <attila.habensusz@gmail.com>
2019-01-21 15:29:57 +0100

    Completed array length task

---
+++ Week-02/Day-01/compare-length.ts
@@ -0,0 +1,8 @@
+let firstList = [1, 2, 3];
+let secondList = [4, 5];
+
+if (firstList.length > secondList.length) {
+    console.log("Firstlist has more elements than secondlist")
+} else {
+    console.log("Secondlist has more elements than firstlist")
+}
