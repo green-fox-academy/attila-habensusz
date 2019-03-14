b8863ba686949065aefed92586ce0cd0d73e3e41
AttilaH <attila.habensusz@gmail.com>
2019-03-14 08:59:29 +0100

    modified movie js file, to make it not bundle movies

--- Week-08/Day-03/Movie selector/main.js
+++ Week-08/Day-03/Movie selector/main.js
@@ -1,10 +1,40 @@
-const genres = document.getElementById('myform');
+const genre = document.getElementById('myform');
 let rightSelect = document.getElementById("tohide");
-rightSelect.options.length=0;
+/* rightSelect.options.length=0; */
+let display = document.querySelector(" div h3");
+
+
+let genres = [
+  ["scifi", "Moon", "2001 Space odessy", "Contact",], 
+  ["drama", "Darkest hour", "There will be blood", "American beauty"], 
+  ["comedy", "Airplane!", "Deadpool", "Wayne's World"]
+];
+
+genre.addEventListener('change', () => {
+  while (rightSelect.childElementCount > 1) {
+    rightSelect.removeChild(rightSelect.lastChild)
+  }
+  genres.forEach(list => {
+    if (list[0] === genre.value) {
+      list.forEach(element => {
+        if (list.indexOf(element) !== 0) {
+          let newElement = document.createElement('option');
+          newElement.setAttribute('value', element);
+          newElement.innerText = element;
+          rightSelect.appendChild(newElement);
+        }
+      });
+    }
+  });
+});
+
+rightSelect.addEventListener('change', () => {
+  display.innerText = `The selected movie is: ${rightSelect.value}`
+});
 
 
 
-genres.addEventListener('change', (e) => {
+/* genres.addEventListener('change', (e) => {
   console.log(`e.target.value = ${ e.target.value }`);
   console.log(`genres.options[genres.selectedIndex].value = ${ genres.options[genres.selectedIndex].value }`);
   if (genres.options[genres.selectedIndex].value === "drama") {
@@ -23,7 +53,7 @@ genres.addEventListener('change', (e) => {
   option5.value = "darkest";
   document.getElementById("tohide");
   select.appendChild(option5);
-  
+  movieDisplay.textContent = e.target.value;
   }
   if (genres.options[genres.selectedIndex].value === "scifi") {
     let option3 = document.createElement("option");
@@ -44,4 +74,4 @@ genres.addEventListener('change', (e) => {
       let select = document.getElementById("tohide");
       select.appendChild(option1);
 }}
-);
+); */
