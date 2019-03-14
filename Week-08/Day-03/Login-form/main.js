b9f10a8855e6e6eeec3bfc7055090ada0826524e
AttilaH <attila.habensusz@gmail.com>
2019-03-14 08:58:51 +0100

    changed stuff  in the login exercise

--- Week-08/Day-03/Login-form/main.js
+++ Week-08/Day-03/Login-form/main.js
@@ -9,28 +9,28 @@ function showOrHide () {
   }
 };
 
-showPassword.addEventListener("change", showOrHide);
+showPassword.addEventListener("click", showOrHide);
 
 let myInput = document.getElementById('password');
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
+let letter = document.getElementById("letter");
+let capital = document.getElementById("capital");
+let number = document.getElementById("number");
+let length = document.getElementById("length");
 
-// When the user clicks on the password field, show the message box
+
 myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
+  document.getElementById("message").style.display = "flex";
 }
 
-// When the user clicks outside of the password field, hide the message box
+
 myInput.onblur = function() {
   document.getElementById("message").style.display = "none";
 }
 
-// When the user starts to type something inside the password field
+
 myInput.onkeyup = function() {
   // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
+  let lowerCaseLetters = /[a-z]/g;
   if(myInput.value.match(lowerCaseLetters)) { 
     letter.classList.remove("invalid");
     letter.classList.add("valid");
@@ -40,7 +40,7 @@ myInput.onkeyup = function() {
 }
 
   // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
+  let upperCaseLetters = /[A-Z]/g;
   if(myInput.value.match(upperCaseLetters)) { 
     capital.classList.remove("invalid");
     capital.classList.add("valid");
@@ -50,7 +50,7 @@ myInput.onkeyup = function() {
   }
 
   // Validate numbers
-  var numbers = /[0-9]/g;
+  let numbers = /[0-9]/g;
   if(myInput.value.match(numbers)) { 
     number.classList.remove("invalid");
     number.classList.add("valid");
